57f2e8182ba62f8d0b82a9bca54cd03752687f28
Tipso <tipson@live.ru>
2022-12-11 23:06:28 +0300

    Используется дополнительно функция из Utills (не добавлена)

---
+++ src/WebUI/Controllers/FilesController.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Synopsis.Infrastructure;
+using Synopsis.Models.Commands.Error;
+using Synopsis.Utils;
+using SynopsisV2.Application.Errors.Commands.CreateError;
+using SynopsisV2.WebUI.Controllers;
+
+namespace Synopsis.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilesController : ApiControllerBase
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    
+    public FilesController(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+    // GET
+
+    [HttpPost("[action]")]
+    public async Task<ActionResult> UploadFile([FromQuery] string f, CancellationToken token = default)
+    {
+        string ret = "";
+        try
+        {
+            foreach (IFormFile file in Request.Form.Files)
+            {
+                
+                string fName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim().ToString()
+                    .ToLower();
+                if (file.Length > 0 &&
+                    (fName.EndsWith(".png") || fName.EndsWith(".jpg") || fName.EndsWith(".jpeg") ||
+                     fName.EndsWith(".gif")) && file.Length < 20 * 1024 * 1024)
+                {
+                    // get a stream
+                    
+                    // and optionally write the file to disk
+                    var filePath = _webHostEnvironment.WebRootPath + ("/images/" + f + "/");
+
+                    string fileName = mainFuncs.GenerateCode(25) +
+                                      fName.Substring(fName.IndexOf("."), fName.Length - fName.IndexOf(".")); //todo Create Utills mainFyuncs
+                    string pathTemp = filePath + fileName;
+
+                    await using (var fileStream = new FileStream(pathTemp, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream, token);
+                    }
+
+                    ret = fileName;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            if (ex.Source != null)
+                await Mediator.Send(
+                    new CreateErrorCommand(ex.Message, ControllerContext.ActionDescriptor.ControllerName,
+                        ControllerContext.ActionDescriptor.ActionName,
+                        ex.Source),
+                    token);
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return Ok("Upload failed");
+        }
+
+        return Ok(ret);
+    }
+}
