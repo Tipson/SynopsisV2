8942a7ac9572a53b581ad78af75a6ad0288d137f
Tipso <tipson@live.ru>
2022-12-16 20:49:04 +0300

    ConfigureServices.cs new service

--- src/Domain/Entities/Logo.cs
+++ src/Domain/Entities/Logo.cs
@@ -1,18 +1,14 @@
-using Synopsis.Infrastructure.DbContext.Entities;
-
 namespace SynopsisV2.Domain.Entities;
 
 public class Logo
 {
     public int SpeakerId { get; set; }
-    public Speaker Speaker { get; set; }
+    public Speaker? Speaker { get; set; }
     public string LogoPath { get; set; }
 
-    public Logo(
-        int speakerId,
-        string logo)
+    public Logo(int speakerId, string logo)
     {
-        SpeakerId = speakerId;
+        SpeakerId = speakerId;   
         LogoPath = logo;
     }
 }
--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -29,6 +29,7 @@ public static class ConfigureServices
         services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
 
         services.AddScoped<ApplicationDbContextInitialiser>();
+        services.AddScoped<ISmtpService, SmtpService>();
 
         services
             .AddDefaultIdentity<ApplicationUser>()
